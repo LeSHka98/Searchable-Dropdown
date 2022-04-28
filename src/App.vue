7218a7c6360fdaa3cf942d89a227bec8f4818c23
Alexei Petrovskiy <pas1-by@yandex.ru>
2022-04-28 11:18:01 +0300

    dropdown latest

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <Dropdown 
-    :items="items"
-    :search="true"
-    :multiple="true"
-    v-model="income"/>
+  <div>
+    <h2>{{income}}</h2>
+
+    <Dropdown 
+      class="dropdownSelect"
+      :items="items"
+      :search="true"
+      :multiple="true"
+      :nullable="true" 
+      v-model:choose="income"/>
+      <!-- <Dropdown 
+      :items="DATA_SET_2"   //   :items="load"
+      :search="true"
+      :multiple="true"
+      :nullable="true"
+      :display="displaySET2"  
+      v-model:choose="income"/> -->
+  </div>
 </template>
 
 <script lang="ts">
@@ -16,17 +29,62 @@ import Dropdown from './components/Dropdown.vue';
   },
 })
 export default class App extends Vue {
-  items = ['мама', 'папа', 'лёша', 'митя', 'андрей', 'петя',
-  'петя','петя','петя','петя','петя','петя','петя','петя','петя','петя','петя','петя','петя','петя','петя',]
 
-  // items = [1, 2, 3, 4, 5, 6,7,8,8,10,11,12,13,14,15,16,17,18,19,20]
+  income = []
+  items = ['сергей', 'ира', 'лёша', 'митя', 'андрей', 'петя',
+  'слава','вася','настя','алла','коля','игорь','данила','матвей','наташа','василиса','егор','валера','костя','женя','никита']
+  items2 = [1, 2, 3, 4, 5, 6,7,8,8,10,11,12,13,14,15,16,17,18,19,20]
+  DATA_SET_2 = [
+    { id: 203, name: 'Андрей Стаселько'},
+    { id: 200, name: 'Валентина Гелясина'},
+    { id: 185, name: 'Виктория Апетёнок'},
+    { id: 192, name: 'Владислав Романов'},
+    { id: 196, name: 'Елена Рыштакова'},
+    { id: 347, name: 'Захар Журавлёв'},
+    { id: 186, name: 'Ирина Толкачёва'},
+    { id: 296, name: 'Наталья Нестерович'},
+    { id: 188, name: 'Парфенова Раиса'},
+    { id: 184, name: 'Татьяна Сарокина'},
+    { id: 215, name: 'Андрей Степин'},
+    { id: 216, name: 'Анна Сулима'},
+    { id: 213, name: 'Артём Карпук'},
+    { id: 261, name: 'Валерий Матусевич'},
+    { id: 207, name: 'Дина Богатко'},
+    { id: 210, name: 'Елена Кицило'},
+    { id: 274, name: 'Елизавета Каленкович'},
+    { id: 208, name: 'Ирина Кондрасюк'},
+    { id: 211, name: 'Маргарита Четверик'},
+    { id: 209, name: 'Наталья Друтько'}
+];
+
+
+  load( val: string): Promise<any> {  
+    val = val?.toLowerCase()                       // DATA_SET_2 изменить по ситуации
+    return new Promise((resolve) => resolve(this.DATA_SET_2.filter((item: any) => { 
+      if(typeof(item) !== 'object') return String(item).toLowerCase().includes(val)
+                // displaySET2 изменить по ситуации
+      return this.displaySET2(item).toLowerCase().includes(val) }   )))
+  }
+  // displaySET2 изменить по ситуации
+  displaySET2(item: {id:number, name: string}) {
+    return item?.name
+  }
+
+  
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   * {
     padding: 0;
     margin: 0;
     font-family: sans-serif;
+  }
+  #app {
+    padding: 10px;
+  }
+
+  .dropdownSelect {
+    margin: 20px auto;
   }
 </style>
--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,87 +1,168 @@
 <template>
   <div 
-    class="select"
-    :class="{select__active: open }"
-    @click="openSelect">
-    <span >{{value}}</span>
+      class="select"
+      :class="{select__active: open, select__nullable: nullableMark }"
+      v-click-outside="close"
+      @click="openSelect">
+    <span v-if="!display" class="select__result">
+      {{result || resultArray[0] || (nullableMark ? "field can't be empty":'choose variants')}}
+    </span>
+    <span v-else class="select__result">
+      {{display(result) || display(resultArray[0]) || (nullableMark ? "field can't be empty":'choose variants')}}
+    </span>
     <div
-      v-if="!valarray.length" 
+      v-if="!resultArray.length || !open" 
       class="select__arrow"
       :class="{select__arrow__active: open }"
-      >^</div>
+      >
+        <svg width="13" height="7" viewBox="0 0 13 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M7.10793 6.76714C6.78181 7.07762 6.25307 7.07762 5.92696 6.76714L0.244587 1.35721C-0.0815288 1.04673 -0.0815289 0.543341 0.244586 0.232861C0.570703 -0.07762 1.09944 -0.0776205 1.42556 0.23286L6.50041 5.0644L11.5744 0.233644C11.9006 -0.0768371 12.4293 -0.0768371 12.7554 0.233644C13.0815 0.544124 13.0815 1.04751 12.7554 1.35799L7.23227 6.61633C7.1981 6.67005 7.15666 6.72075 7.10793 6.76714Z" fill="#898989"/>
+        </svg>
+      </div>
     <div
-      v-else 
+      v-else-if="open" 
       class="select__cross"
       @click ="clear"
-    >X</div>
+    >
+      <img src="@/assets/close.png" alt="close">
+    </div>
     <div class="dropdown" v-if="open">
       <input v-if="search" class="dropdown__search" v-model="searchLine" type="text" placeholder="Search">
       <ul class="dropdown__list">
-        <li 
-          class="dropdown__item"
-          :class="{dropdown__selected: this.valarray.includes(item)}"
-          v-for="item of localItems" 
-          :key="item"
-          @click="mark(item)" 
+        <div v-if="!multiple">
+          <li 
+            class="dropdown__item"
+            :class="{dropdown__selected: item == result}"
+            v-for="item of localItems" 
+            :key="item"
+            @click="mark(item)" 
          >
-          <input v-if="multiple" class="dropdown__checkbox" type="checkbox" :value="item" :id="item" v-model="valarray">
-          <label :for="item">{{ item }}</label>
-        </li>
+         {{ display ? display(item) : item  }}
+         </li>
+        </div>
+        <div v-else>
+          <label v-for="item of localItems" :key="item" :for="item.id || item" class="dropdown__item"
+                :class="{dropdown__selected: resultArray.includes(item)}">
+            <input class="dropdown__checkbox" type="checkbox" :value="item" :id="item.id || item" v-model="resultArray">
+            {{ display ? display(item) : item }}
+          </label>
+        </div>
       </ul>
     </div>
   </div>
-  {{valarray}}
 </template>
 
 <script lang="ts">
-import {  Vue } from 'vue-class-component';
+import { Options, Vue } from 'vue-class-component';
 import { Prop, Watch } from 'vue-property-decorator'
+import vClickOutside from 'click-outside-vue3'
 
+@Options({
+  directives: {
+    clickOutside: vClickOutside.directive
+  }
+})
 export default class Dropdown extends Vue {
-  @Prop() items!: any[]|((val: string) => Promise<any>)
-  @Prop(Boolean) multiple: boolean | undefined
-  @Prop(Boolean) search: boolean | undefined
+  @Prop({ required: true }) items!: any[]|((val: string) => Promise<any>)
+  @Prop({Boolean, required: true }) multiple!: boolean
+  @Prop({Boolean, required: true }) search!: boolean 
+  @Prop(Boolean) nullable: boolean | undefined
+  @Prop() display: ((val: any) => string) | undefined
+
+  open = false
+  nullableMark = false 
+  searchLine = ''
+  result!: any 
+  resultArray: any[] = []
+  localItems: any[] = []
 
   @Watch('searchLine')
   async onsearchLineChange(val: string) {
-    if (typeof(this.items) !== 'function') this.localItems = this.items.filter(item => String(item).includes(val))
+    val = val?.toLowerCase()
+    if (typeof(this.items) !== 'function') this.localItems = this.items.filter(item => {
+      if(typeof(item) === 'object' && this.display) return this.display(item).toLowerCase().includes(val)
+      return String(item).toLowerCase().includes(val)
+    })
     else this.localItems = await this.items(val)
-  }
-
-  open = false 
-  searchLine = ''
-  value!: any | any[]
-  valarray: any[] = []
-  localItems: any[] = []
-
-  mounted() {
-    if(this.items.length > 20 && !this.search && typeof(this.items) !== 'function') { 
-      this.items.length = 20 
-      this.localItems = this.items
+  } 
+  @Watch('resultArray')
+  onresultArrayChange(val:any) {
+    if(typeof(val[0]) === 'object' ){
+      this.$emit('update:choose', this.resultArray.map(item => item.id))
     }
-    if (typeof(this.items) !== 'function') this.localItems = this.items
+    else this.$emit('update:choose', this.resultArray)
   }
-  
-  mark(item: any) {
-    if (!this.multiple) {
-      if(this.value !== item) this.value = item
-      else this.value = null
+
+  @Watch('open')  // пункт 4 - только отмеченные элементы после открытия
+  async onOpenChange(val: boolean) {
+    if (this.multiple) {
+      this.markNull(val, this.resultArray.length)
+
+      if (typeof(this.items) !== 'function') {
+        if(val && this.resultArray.length > 0) this.localItems = this.items.filter(item => [...this.resultArray].includes(item))
+        else this.localItems = [...this.items]
+      }
+      else {
+        if(val && this.resultArray.length > 0) { 
+          this.localItems = (await this.items('')).filter((item:any) => [...this.resultArray].includes(item))  }
+        else this.localItems = await this.items('')
+      }
     }
     else {
-      console.log(this.valarray)
+      this.markNull(val, this.result)
+      if (typeof(this.items) !== 'function') {
+        if(val && Boolean(this.result)) { 
+          this.localItems.length = 0; 
+          this.localItems[0] = this.items.find(item => this.result == item);
+        }
+        else  this.localItems = [...this.items]
+      }
+      else {
+        if(val && Boolean(this.result)) { 
+          this.localItems.length = 0; 
+          this.localItems[0] = (await this.items('')).find((item:any) => this.result == item);
+        }
+        else  this.localItems = await this.items('')
+      }
     }
+    if(!val) this.searchLine = ''
+  }
+
+  mounted() {
+    if(this.items.length > 20 && typeof(this.items) !== 'function') { 
+      this.items.length = 20 
+      this.localItems = [...this.items]
+    }
+    if (typeof(this.items) !== 'function' && typeof(this.items[0]) === 'object' && !this.display) {
+      alert('For this type of data Display function should be provided') }
+    else if(typeof(this.items) !== 'function' && typeof(this.items[0]) !== 'object' && this.display) {
+      alert('For this type of data Display function should not be provided') }
+  }
+  
+  markNull(val: boolean, result: any) {
+    if(this.nullable && !val && !result) this.nullableMark = true
+    else this.nullableMark = false
+  }
+
+  mark(item: any) {
+    if(this.result !== item) this.result = item
+    else this.result = null
+    this.$emit('update:choose', this.result?.id || this.result)
   }
 
   openSelect(e: any) {
     if (!this.multiple) {
       if(e.target.className !== 'dropdown__search') this.open = !this.open
     }
-    else if(e.target.className == 'select' || e.target.className == 'select select__active') this.open = !this.open
+    else if(Array.from(e.target.classList).includes('select')) this.open = !this.open
   }
-
+  close() {
+    return this.open = false
+  }
   clear() {
-    this.value = null
-    this.valarray = []
+    this.result = null
+    this.resultArray = []
+    if (typeof(this.items) !== 'function') this.localItems = [...this.items]
   }
 }
 </script>
@@ -93,16 +174,20 @@ export default class Dropdown extends Vue {
     border: 1px solid #C4C4C4;
     border-radius: 5px;
     box-sizing: border-box;
-    margin: auto;
     display: flex;
     align-items: center;
     position: relative;
+    cursor: pointer;
 
     &__active {
       border: 1px solid #86D4E8;
     }
-    span {
+    &__nullable {
+      border: 1px solid red;
+    }
+    &__result {
       margin-left: 7px;
+      color: #888888;
     }
     &__arrow,
     &__cross {
@@ -110,18 +195,20 @@ export default class Dropdown extends Vue {
       width: 12px;
       height: 5px;
       right: 10px;
-      top: calc(50% - 7px/2 + 5.5px);
+      top: calc(50% - 3.5px + 8.5px);
       color: #898989;
       transform: matrix(1, 0, 0, -1, 0, 0);
-      cursor: pointer;
+      
+       &__active {
+        transform: none;
+        top: calc(50% - 9px);
+      }
     }
       &__cross {
         top: calc(50% - 7px/2 + 8px);
-      }
-
-      &__active {
-        transform: none;
-        top: calc(50% - 5px);
+        img {
+          width: 13px;
+        }
       }
     }
   
@@ -135,6 +222,8 @@ export default class Dropdown extends Vue {
 
     &__list {
       max-height: 200px;
+      padding: 0;
+      margin: 0;
       overflow-y: scroll;
       overflow-x: hidden;
     }
